f4c3bdf2ea13dd4fc451f33184493702de173202
Aaliyah Harper <aeverly14@pm.me>
2025-03-29 21:04:14 -0400

    hardcode SNI for debugging

--- config/runtime.exs
+++ config/runtime.exs
@@ -21,8 +21,6 @@ if System.get_env("PHX_SERVER") do
 end
 
 if config_env() == :prod do
-  db_schema = System.get_env("DB_SCHEMA") || raise "environment variable DB_SCHEMA is missing"
-
   database_url =
     System.get_env("DATABASE_URL") ||
       raise """
@@ -36,16 +34,12 @@ if config_env() == :prod do
     config :vyre, Vyre.Repo,
       ssl: [
         verify: :verify_peer,
-        cacerts: :public_key.cacerts_get()
+        cacertfile: "/etc/ssl/certs/prod-ca-2021.crt",
+        server_name_indication: "aws-0-us-east-1.pooler.supabase.com"
       ],
-      parameters: [search_path: db_schema],
-      migration_default_prefix: db_schema,
-      migration_source: "#{db_schema}_schema_migrations",
       url: database_url,
-      pool_size: String.to_integer(System.get_env("POOL_SIZE") || "5"),
-      queue_target: 5000,
-      queue_interval: 5000,
-      socket_options: [:inet]
+      pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+      socket_options: maybe_ipv6
   end
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
@@ -76,6 +70,38 @@ if config_env() == :prod do
       port: port
     ],
     secret_key_base: secret_key_base
+
+  # ## SSL Support
+  #
+  # To get SSL working, you will need to add the `https` key
+  # to your endpoint configuration:
+  #
+  #     config :vyre, VyreWeb.Endpoint,
+  #       https: [
+  #         ...,
+  #         port: 443,
+  #         cipher_suite: :strong,
+  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
+  #       ]
+  #
+  # The `cipher_suite` is set to `:strong` to support only the
+  # latest and more secure SSL ciphers. This means old browsers
+  # and clients may not be supported. You can set it to
+  # `:compatible` for wider support.
+  #
+  # `:keyfile` and `:certfile` expect an absolute path to the key
+  # and cert in disk or a relative path inside priv, for example
+  # "priv/ssl/server.key". For all supported SSL configuration
+  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
+  #
+  # We also recommend setting `force_ssl` in your config/prod.exs,
+  # ensuring no data is ever sent via http, always redirecting to https:
+  #
+  #     config :vyre, VyreWeb.Endpoint,
+  #       force_ssl: [hsts: true]
+  #
+  # Check `Plug.SSL` for all available options in `force_ssl`.
 
   # ## Configuring the mailer
   #
